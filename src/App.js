812fcf13a00aaeb461ae14d564a1d168282f14b9
kato-syogo <kato-syogo@dmm.com>
2019-01-06 18:05:25 +0900

    setup graphql client

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import {ApolloProvider} from 'react-apollo';
+import {gql} from 'apollo-boost';
+import {Query} from 'react-apollo';
+import client from './client';
 
-const GIHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-console.log(GIHUB_TOKEN);
+const ME = gql`
+  query me {
+    user(login: "kt-sygo0921") {
+      name
+      avatarUrl
+    }
+  }
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>Hello GraphQL</div>
+      <ApolloProvider client={client}>
+        <div>Hello GraphQL</div>
+        <Query query={ME}>
+          {
+            ({loading, error, data}) => {
+              if(loading) return 'loading...';
+              if(error) return `Error!${error.message}`;
+              return <div>{data.user.name}</div>
+            }
+          }
+        </Query>
+      </ApolloProvider>
     );
   }
 }
