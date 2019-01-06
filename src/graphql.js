824e07e1d4884741d5df7a50459e105c7dc23d7a
kato-syogo <kato-syogo@dmm.com>
2019-01-06 18:37:20 +0900

    apply query and variables

--- src/graphql.js
+++ src/graphql.js
@@ -1,5 +1,32 @@
 import {gql} from 'apollo-boost';
 
+export const SEARCH_REPOSITORIES = gql`query searchRepositories($first:Int, $last:Int, $before:String, $after:String, $query:String!) {
+    search(first:$first, last:$last, before:$before, after:$after, query:$query, type:REPOSITORY) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            id
+            name
+            url
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
+    }
+  }
+`;
+
 export const ME = gql`
   query me {
     user(login: "kt-sygo0921") {
