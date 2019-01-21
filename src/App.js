5cc1f87b10e16e722ba91c1b86fd25cb5177a6be
kato-syogo <kato-syogo@dmm.com>
2019-01-21 20:14:34 +0900

    add show stargazers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import {Query} from 'react-apollo';
 import client from './client';
 import {SEARCH_REPOSITORIES} from './graphql';
 
+const StartButton = props => {
+  console.log(props.node.stargazers.totalCount)
+  const totalCount = props.node.stargazers.totalCount;
+  return <button>{totalCount === 1 ? '1 star' : `${totalCount} starts`}</button>
+}
+
 const PER_PAGE = 5;
 const DEFAULT_STATE = {
   first: PER_PAGE,
@@ -74,6 +80,8 @@ class App extends Component {
                         return (
                           <li key={node.id}>
                             <a href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
+                            &nbsp;
+                            <StartButton node={node} />
                           </li>
                         )
                       })
